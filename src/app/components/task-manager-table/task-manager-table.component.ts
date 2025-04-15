e399fdc5e5318bbc25e02206bb085ca4c4ab36ea
Marcos Vinicius  Ribeiro Oliveira <marcosviniciusribeirooliveira@MacBook-Pro-de-Marcos.local>
2025-04-15 15:44:25 -0300

    initial-commit

---
+++ src/app/components/task-manager-table/task-manager-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-manager-table',
+  imports: [],
+  templateUrl: './task-manager-table.component.html',
+  styleUrl: './task-manager-table.component.scss'
+})
+export class TaskManagerTableComponent {
+
+}
---
+++ src/app/pages/task-manager-list/task-manager-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-manager-list',
+  imports: [],
+  templateUrl: './task-manager-list.component.html',
+  styleUrl: './task-manager-list.component.scss'
+})
+export class TaskManagerListComponent {
+
+}
